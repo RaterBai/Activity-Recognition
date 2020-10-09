87c627fd857d037a32191aa0bc954942377ddbe0
Chen Bai <chenbai@ufl.edu>
2020-10-09 12:25:02 -0400

    add window length

--- code/data_extraction.py
+++ code/data_extraction.py
@@ -7,6 +7,10 @@ import os
 import re
 import matplotlib.pyplot as plt
 
+# frequence of the accelerometer data
+FREQ = 30 
+WINDOW_LENGTH = 10
+
 accel_dir = "./../data/mock study/"
 activity_dir = "./../data/mock study/Participant/"
 
@@ -36,10 +40,36 @@ def selectPeriod(start, end, input):
     end = pd.to_datetime(end, format="%d/%m/%Y %H:%M:%S%z")
     return input[(input['timestamp'] > start) & (input['timestamp'] <= end)].copy()
 
+def crop_by_window(data, window):
+    length = window * FREQ
+    data_list = []
+    # if the last one miss more than 10%, remove them
+    print("data.shape[0] : %d    length : %d" %(data.shape[0], length))
+    if(data.shape[0]%length < length*9/10):
+        for i in range(data.shape[0]//length):
+            tmp = data[i*length: (i+1)*length].copy()
+            tmp['Index'] = i
+            tmp.reset_index()
+            data_list.append(tmp)
+    else:
+        for i in range(int(np.ceil(data.shape[0]/length))):
+            if(i != np.ceil(data.shape[0]/length)-1):
+                tmp = data[i*length:(i+1)*length].copy()
+            else:
+                tmp = data[i*length:].copy()
+            tmp['Index'] = i
+            tmp.reset_index()
+            data_list.append(tmp)
+    retval = pd.concat(data_list)
+    return(retval.copy())
+
+
 for file in activity_files:
+    #print(file)
     # read time 
     # participant data
     pData = pd.read_csv("./../data/mock study/Participant/" + file)
+    #print(pData)
     # create patient data by cropping time
     data_list = []
     for i in range(pData.shape[0]):
@@ -50,6 +80,20 @@ for file in activity_files:
         data_list.append(newData)
     # selected participant data
     patData = pd.concat(data_list)
+    print(patData.head())
+    # apply window-length to crop data
+    # For each activity 
+    act_list = []  # activity list
+    activities = np.unique(patData.ActivityNumber)
+
+    for activity in activities:
+        tmp = patData.query("ActivityNumber==%s"%str(activity))
+        retval = crop_by_window(tmp, WINDOW_LENGTH)
+        act_list.append(retval)
+
+    finalData = pd.concat(act_list)
+
+    finalData.to_csv(save_dir + file.split(".")[0] + "_final.csv", index = False)
     patData.to_csv(save_dir + file.split(".")[0] + ".csv", index = False)
 
 
